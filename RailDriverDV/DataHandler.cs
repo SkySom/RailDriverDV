15c900fc7e099d4f335e6f05832ebdf279e2c490
Skylar <skylar.som@gmail.com>
2022-01-03 00:12:34 -0500

    Horn Sorta works
    Doesn't let normal engine effect it.

---
+++ RailDriverDV/DataHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CommandTerminal;
+using PIEHid64Net;
+using UnityEngine;
+
+namespace RailDriverDV
+{
+    public class DataHandler: PIEDataHandler, PIEErrorHandler
+    {
+        public void HandlePIEHidData(byte[] data, PIEDevice sourceDevice, int error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandlePIEHidError(PIEDevice sourceDevices, long error)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RailDriverDV/Main.cs
@@ -0,0 +1,61 @@
+﻿using CommandTerminal;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace RailDriverDV
+{
+    [EnableReloading]
+    public static class Main
+    {
+        [CanBeNull] private static RailDriver _railDriver;
+        [CanBeNull] private static GameObject _rootObject;
+
+        [UsedImplicitly]
+        public static bool Load(UnityModManager.ModEntry entry)
+        {
+            entry.OnToggle = OnToggle;
+            return true;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry entry, bool active)
+        {
+            if (active)
+            {
+                _railDriver?.Dispose();
+                var newRailDriver = RailDriver.Setup();
+                if (newRailDriver == null)
+                {
+                    entry.Logger.Error("Failed to Setup RailDriver");
+                    return false;
+                }
+                
+                Debug.Log(newRailDriver.ProductString());
+
+                _railDriver = newRailDriver;
+
+                if (_rootObject != null) return true;
+                
+                _rootObject = new GameObject();
+                Object.DontDestroyOnLoad(_rootObject);
+                _rootObject.AddComponent<LocoControl>();
+                var locoControl = _rootObject.GetComponent<LocoControl>();
+                locoControl.RailDriver = _railDriver;
+                locoControl.Start();
+            }
+            else
+            {
+                if (_rootObject != null)
+                {
+                    var locoControl = _rootObject.GetComponent<LocoControl>();
+                    locoControl.Stop();
+                    Object.Destroy(_rootObject);
+                }
+                
+                _railDriver?.Dispose();
+                _railDriver = null;
+            }
+            return true;
+        }
+    }
+}
